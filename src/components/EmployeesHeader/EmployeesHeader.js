59c1affb07de3066ac3db2dfef4a4296e8488953
Viacheslav Liashenko <87814580+vliashenko@users.noreply.github.com>
2022-07-02 22:05:17 +0300

    Add files via upload

--- src/components/EmployeesHeader/EmployeesHeader.js
+++ src/components/EmployeesHeader/EmployeesHeader.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import logo from "../../resourses/img/logoMain.png";
 import styles from "../Header/Header.module.css"
 
 const EmployeeHeader = () => {
+
+    const [ sideMenu, setSideMenu ] = useState(false);
+
+    const onClickHandler = () => {
+        setSideMenu(sideMenu => !sideMenu)
+    }
+
+    const onCloseHandler = () => {
+        setSideMenu(false)
+    }
+
     return (
+        <>
+        <div id="mySidenav" style={ sideMenu === true ? {width: "250px"} : {width: "0"}} className={styles.sidenav}>
+            <span className={styles.closebtn} onClick={onCloseHandler}>&times;</span>
+            <p className={styles.sidenavLink}>
+                <Link className={styles.link} to="/">
+                    Головна
+                </Link>
+            </p>
+            <p className={styles.sidenavLink}>
+                <Link className={styles.link} to="/masters">
+                    Майстри
+                </Link>
+            </p>
+            <p className={styles.sidenavLink}>
+            <Link className={styles.link} to="/pricelist">
+                Ціни
+            </Link>
+            </p>
+            <p className={styles.sidenavLink}>
+                <Link className={styles.link} to="/contacts">
+                    Контакти
+                </Link>
+            </p>
+        </div>
+
         <header className={styles.EmployeeHeader}>
             <div className={styles.headerTopEmployee}>
             
             <img src={logo} alt="main-logo" className={styles.logoMainHidden} />
-            <i className={`fa-solid fa-bars-staggered ${styles.burger}`}></i>
+            <i onClick={onClickHandler} className={`fa-solid fa-bars-staggered ${styles.burger}`}></i>
 
                 <ul className={styles.headerTopList}>
                     <li className={styles.headerTopListItem}>
@@ -40,6 +76,7 @@ const EmployeeHeader = () => {
                 </ul>
             </div>
         </header>
+        </>
     );
 };
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,66 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import logo from "../../resourses/img/logoMain.png";
 import mouse from "../../resourses/img/mouseHeaderMain.png"
 import styles from "./Header.module.css";
 
 const Header = () => {
-    return (
-        <header className={styles.header}>
-            <div className={styles.headerTop}>
-            
-            <img src={logo} alt="main-logo" className={styles.logoMainHidden} />
-            <i className={`fa-solid fa-bars-staggered ${styles.burger}`}></i>
 
-                <ul className={styles.headerTopList}>
-                    
-                        <li className={styles.headerTopListItem}>
-                        <Link className={styles.link} to="/">
-                            Головна
-                        </Link>
-                        </li>
-                    
-                        <li className={styles.headerTopListItem}>
-                            <Link className={styles.link} to="/masters">
-                                Майстри
+    const [ sideMenu, setSideMenu ] = useState(false);
+
+    const onClickHandler = () => {
+        setSideMenu(sideMenu => !sideMenu)
+    }
+
+    const onCloseHandler = () => {
+        setSideMenu(false)
+    }
+
+    return (
+        <>
+        <div id="mySidenav" style={ sideMenu === true ? {width: "250px"} : {width: "0"}} className={styles.sidenav}>
+            <span className={styles.closebtn} onClick={onCloseHandler}>&times;</span>
+            <p className={styles.sidenavLink}>
+                <Link className={styles.link} to="/">
+                    Головна
+                </Link>
+            </p>
+            <p className={styles.sidenavLink}>
+                <Link className={styles.link} to="/masters">
+                    Майстри
+                </Link>
+            </p>
+            <p className={styles.sidenavLink}>
+            <Link className={styles.link} to="/pricelist">
+                Ціни
+            </Link>
+            </p>
+            <p className={styles.sidenavLink}>
+                <Link className={styles.link} to="/contacts">
+                    Контакти
+                </Link>
+            </p>
+        </div>
+
+            <header className={styles.header}>
+                <div className={styles.headerTop}>
+                
+                <img src={logo} alt="main-logo" className={styles.logoMainHidden} />
+                <i onClick={onClickHandler} className={`fa-solid fa-bars-staggered ${styles.burger}`}></i>
+
+                    <ul className={styles.headerTopList}>
+                        
+                            <li className={styles.headerTopListItem}>
+                            <Link className={styles.link} to="/">
+                                Головна
+                            </Link>
+                            </li>
+                        
+                            <li className={styles.headerTopListItem}>
+                                <Link className={styles.link} to="/masters">
+                                    Майстри
+                                </Link>
+                            </li>
+                        
+                        <li >
+                            <Link className={styles.link} to="/">
+                                <img src={logo} alt="main-logo" className={styles.logoMain} />
                             </Link>
                         </li>
-                    
-                    <li >
-                        <Link className={styles.link} to="/">
-                            <img src={logo} alt="main-logo" className={styles.logoMain} />
-                        </Link>
-                    </li>
-                    <li className={styles.headerTopListItem}>
-                        <Link className={styles.link} to="/pricelist">
-                            Ціни
-                        </Link>
-                    </li>
-                    <li className={styles.headerTopListItem}>
-                        <Link className={styles.link} to="/contacts">
-                            Контакти
-                        </Link>
-                    </li>
-                </ul>
-            </div>
+                        <li className={styles.headerTopListItem}>
+                            <Link className={styles.link} to="/pricelist">
+                                Ціни
+                            </Link>
+                        </li>
+                        <li className={styles.headerTopListItem}>
+                            <Link className={styles.link} to="/contacts">
+                                Контакти
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
 
-            <div className={styles.headerCenter}>
-                <p className={styles.headerCenterItem}>
-                    Салон краси 
-                </p>
-                <p className={styles.headerCenterItem}>
-                    «Dilighna-beauty» 
-                </p>
-                <p className={styles.headerCenterItem}>
-                    на Київській 
-                </p>
-            </div>
+                <div className={styles.headerCenter}>
+                    <p className={styles.headerCenterItem}>
+                        Салон краси 
+                    </p>
+                    <p className={styles.headerCenterItem}>
+                        «Dilighna-beauty» 
+                    </p>
+                    <p className={styles.headerCenterItem}>
+                        на Київській 
+                    </p>
+                </div>
 
-            <div className={styles.headerBottom}>
-                <img src={mouse} alt="mouse" className={styles.headerBottomMouse} />
-                <span className={styles.headerBottomLabel}>Прокрутити вниз</span>
-            </div>
-        </header>
+                <div className={styles.headerBottom}>
+                    <img src={mouse} alt="mouse" className={styles.headerBottomMouse} />
+                    <span className={styles.headerBottomLabel}>Прокрутити вниз</span>
+                </div>
+            </header>
+        </>
     );
 };
 
